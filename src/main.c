5e2fc8e7312a65011785e4946a3c72331c26f055
wltjr0920 <wltjr0920@ajou.ac.kr>
2017-11-09 20:09:08 +0900

    ??

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #include "commands.h"
 #include "built_in.h"
@@ -8,24 +11,24 @@
 
 int main()
 {
-  char buf[8096];
+    char buf[8096];
 
-  while (1) {
-    fgets(buf, 8096, stdin);
+    while (1) {
+	fgets(buf, 8096, stdin);
 
-    struct single_command commands[512];
-    int n_commands = 0;
-    mysh_parse_command(buf, &n_commands, &commands);
+	struct single_command commands[512];
+	int n_commands = 0;
+	mysh_parse_command(buf, &n_commands, &commands);
 
-    int ret = evaluate_command(n_commands, &commands);
+	int ret = evaluate_command(n_commands, &commands);
 
-    free_commands(n_commands, &commands);
-    n_commands = 0;
+	free_commands(n_commands, &commands);
+	n_commands = 0;
 
-    if (ret == 1) {
-      break;
+	if (ret == 1) {
+	    break;
+	}
     }
-  }
 
-  return 0;
+    return 0;
 }
