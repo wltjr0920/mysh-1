0750a2862e875a41bfd4d282a8c9325298d6afc5
wltjr0920 <wltjr0920@ajou.ac.kr>
2017-11-09 20:43:44 +0900

    Process Creation

--- src/commands.c
+++ src/commands.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #include "commands.h"
 #include "built_in.h"
@@ -30,47 +33,56 @@ static int is_built_in_command(const char* command_name)
  */
 int evaluate_command(int n_commands, struct single_command (*commands)[512])
 {
-  if (n_commands > 0) {
-    struct single_command* com = (*commands);
+    if (n_commands > 0) {
+	struct single_command* com = (*commands);
 
-    assert(com->argc != 0);
+	assert(com->argc != 0);
 
-    int built_in_pos = is_built_in_command(com->argv[0]);
-    if (built_in_pos != -1) {
-      if (built_in_commands[built_in_pos].command_validate(com->argc, com->argv)) {
-        if (built_in_commands[built_in_pos].command_do(com->argc, com->argv) != 0) {
-          fprintf(stderr, "%s: Error occurs\n", com->argv[0]);
-        }
-      } else {
-        fprintf(stderr, "%s: Invalid arguments\n", com->argv[0]);
-        return -1;
-      }
-    } else if (strcmp(com->argv[0], "") == 0) {
-      return 0;
-    } else if (strcmp(com->argv[0], "exit") == 0) {
-      return 1;
-    } else {
-      fprintf(stderr, "%s: command not found\n", com->argv[0]);
-      return -1;
+	int built_in_pos = is_built_in_command(com->argv[0]);
+	if (built_in_pos != -1) {
+	    if (built_in_commands[built_in_pos].command_validate(com->argc, com->argv)) {
+		if (built_in_commands[built_in_pos].command_do(com->argc, com->argv) != 0) {
+		    fprintf(stderr, "%s: Error occurs\n", com->argv[0]);
+		}
+	    } else {
+		fprintf(stderr, "%s: Invalid arguments\n", com->argv[0]);
+		return -1;
+	    }
+	} else if (strcmp(com->argv[0], "") == 0) {
+	    return 0;
+	} else if (strcmp(com->argv[0], "exit") == 0) {
+	    return 1;
+	} else if (com->argc>0){
+	    pid_t pid=fork();
+	    if(pid==0){
+		execv(com->argv[0],com->argv);
+		exit(1);
+	    }else{
+		waitpid(pid,0,0);
+	    }
+	}
+       	else {
+	    fprintf(stderr, "%s: command not found\n", com->argv[0]);
+	    return -1;
+	}
     }
-  }
 
-  return 0;
+    return 0;
 }
 
 void free_commands(int n_commands, struct single_command (*commands)[512])
 {
-  for (int i = 0; i < n_commands; ++i) {
-    struct single_command *com = (*commands) + i;
-    int argc = com->argc;
-    char** argv = com->argv;
+    for (int i = 0; i < n_commands; ++i) {
+	struct single_command *com = (*commands) + i;
+	int argc = com->argc;
+	char** argv = com->argv;
 
-    for (int j = 0; j < argc; ++j) {
-      free(argv[j]);
+	for (int j = 0; j < argc; ++j) {
+	    free(argv[j]);
+	}
+
+	free(argv);
     }
 
-    free(argv);
-  }
-
-  memset((*commands), 0, sizeof(struct single_command) * n_commands);
+    memset((*commands), 0, sizeof(struct single_command) * n_commands);
 }
